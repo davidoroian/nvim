369242e4df6a53b06eac4b52f529bfc13b52738b
davidoroian <david.oroian@aalto.fi>
2025-08-14 12:43:04 +0300

    improved metals config

--- lua/daviddavid/set.lua
+++ lua/daviddavid/set.lua
@@ -3,8 +3,8 @@ vim.opt.guicursor = "n-v-c-i:block"
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
