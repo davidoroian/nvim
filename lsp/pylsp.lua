9af9f37a8918e8a33dc9dd5e7bd427d345a9df52
David Oroian <david.oroian@proton.me>
2025-09-01 13:58:35 +0300

    fixed pylsp extensions so that gd, K and C-k work as intended

--- lsp/pylsp.lua
+++ lsp/pylsp.lua
@@ -1,6 +1,6 @@
 return {
 	cmd = {
-		vim.fn.expand('$HOME/anaconda3/bin/pylsp'),
+		vim.fn.expand('$HOME/anaconda3/envs/pylsp-env/bin/pylsp'),
 	},
 	filetypes = {
 		"python",
@@ -20,12 +20,53 @@ return {
 	settings = {
 		pylsp = {
 			plugins = {
+				jedi_definition = {
+					enabled = true,
+					follow_imports = true,
+					follow_builtin_imports = true,
+				},
+				jedi_hover = {
+					enabled = true,
+				},
+				jedi_references = {
+					enabled = true,
+				},
+				jedi_signature_help = {
+					enabled = true,
+				},
+				jedi_symbols = {
+					enabled = true,
+					all_scopes = true,
+				},
+				-- Code formatting and linting
 				ruff = {
 					enabled = true,
+				},
+				-- Disable conflicting plugins if using ruff
+				pycodestyle = {
+					enabled = false,
+				},
+				pyflakes = {
+					enabled = false,
+				},
+				mccabe = {
+					enabled = false,
+				},
+				pylint = {
+					enabled = false,
 				},
 			}
 		}
 	},
 	single_file_support = true,
 	log_level = vim.lsp.protocol.MessageType.Warning,
+	on_attach = function(client, bufnr)
+		-- LSP keymaps
+		local opts = { buffer = bufnr, silent = true }
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+		vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+		vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+	end,
 }
